a1aed982cf812d1a2e26c55e732cf38d150c9dfb
camazog1 <camazog1@eafit.edu.co>
2025-02-13 17:14:59 -0500

    Third Tutorial - Part B - DIP

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, ProductSuccessView, CartView, CartRemoveAllView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, ProductSuccessView, CartView, CartRemoveAllView, ImageViewFactory
+from .utils import ImageLocalStorage
+
+ImageView = ImageViewFactory(ImageLocalStorage())
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -12,4 +16,6 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageView.as_view(), name='image_index'),
+    path('image/save', ImageView.as_view(), name='image_save'),
 ]
